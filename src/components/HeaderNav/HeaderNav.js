af5dbc29da183d714f11920753034ef53a27a7b2
Korobenko2023 <vivika3110@gmail.com>
2023-12-16 23:19:29 +0200

    add task

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,16 +1,16 @@
 import { useSelector } from "react-redux";
 import { Header } from "./HeaderNav.styled";
 import { selectIsLoggedIn } from "redux/auth/selectors";
-// import { Navigation } from "components/Navigation/Navigation";
 import { AuthNav } from "components/AuthNav/AuthNav";
 import { UserMenu } from "components/UserMenu/UserMenu";
+import { Navigation } from "components/Navigation/Navigation";
 
 export const HeaderNav = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
  
   return (
     <Header>
-      {/* <Navigation /> */}
+      <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}      
     </Header>
   );
