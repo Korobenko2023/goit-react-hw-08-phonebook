31ccd45ab5f53e65a918332175d1475017f15bf7
Korobenko2023 <130507694+Korobenko2023@users.noreply.github.com>
2023-12-16 21:54:25 +0000

    Deploying to gh-pages from @ Korobenko2023/goit-react-hw-08-phonebook@945b70812bcdacf3c8e675f13941daaf0f18dac2 🚀

--- static/js/200.45a16d1e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[200],{200:(e,t,n)=>{n.r(t),n.d(t,{default:()=>s});var r,a=n(168);const o=n(867).ZP.h2(r||(r=(0,a.Z)(["\nmargin-top: ",";\ntext-align: center;\ntext-transform: uppercase;\nfont-style: italic;\ncolor: ","; \n\n"])),(e=>e.theme.spasing(6)),(e=>e.theme.colors.Indigo));var c=n(184);function s(){return(0,c.jsx)("div",{children:(0,c.jsx)(o,{children:"Log In"})})}}}]);
-//# sourceMappingURL=200.45a16d1e.chunk.js.map
