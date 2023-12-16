4d2512233607505cae2230ee9a9ae5911789cda5
Korobenko2023 <vivika3110@gmail.com>
2023-12-16 23:11:18 +0200

    add task

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,11 @@
-import { RegisterForm } from "components/RegisterForm/RegisterForm";
+// import { RegisterForm } from "components/RegisterForm/RegisterForm";
 import { RegisterTitle } from "./Register.styled";
 
 export default function Register() {
   return (
     <div>     
         <RegisterTitle>Registration</RegisterTitle>     
-      <RegisterForm />
+      {/* <RegisterForm /> */}
     </div>
   );
 }
