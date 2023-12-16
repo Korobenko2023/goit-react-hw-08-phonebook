f2cb6eef5af215d5351fb49e85fadc10ca2f2ce3
Korobenko2023 <vivika3110@gmail.com>
2023-12-16 23:44:39 +0200

    add Navigation

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -3,38 +3,18 @@ import { Header } from "./HeaderNav.styled";
 import { selectIsLoggedIn } from "redux/auth/selectors";
 import { AuthNav } from "components/AuthNav/AuthNav";
 import { UserMenu } from "components/UserMenu/UserMenu";
-
-
-
+import { Navigation } from "components/Navigation/Navigation";
 
 export const HeaderNav = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
  
   return (
     <Header>
-     {/* <Navigation /> */}
+      <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}      
     </Header>
   );
 };
 
-
-// import { useSelector } from "react-redux";
-// import { selectIsLoggedIn } from "redux/auth/selectors";
-
-
-
-// export const Navigation = () => {
-//    const isLoggedIn = useSelector(selectIsLoggedIn);
-
-//   return (
-//     <Nav>
-//       <NaviLink to="/">Home</NaviLink>
-//       {isLoggedIn && (
-//         <NaviLink to="/contacts">Contacts</NaviLink>
-//       )}
-//     </Nav>
-//   );
-// };
 
 
--- src/components/HeaderNav/HeaderNav.styled.js
+++ src/components/HeaderNav/HeaderNav.styled.js
@@ -11,26 +11,4 @@ box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.12),
 `;
 
 
-// import styled from 'styled-components';
-// import { NavLink } from 'react-router-dom';
-
-// export const Nav = styled.nav`
-//   display: flex;
-//   gap: ${p => p.theme.spasing(5)};
-// `;
-
-// export const NaviLink = styled(NavLink)` 
-//   font-size: 28px;
-//   font-weight: 700;
-//   gap: ${p => p.theme.spasing(5)};
-//   color: ${p => p.theme.colors.Indigo};
-//   font-style: italic; 
-//   transition: color 250ms cubic-bezier(0.4, 0, 0.2, 1);
-//   &:hover,
-//   &:focus,
-//   &.active {
-//     color: ${p => p.theme.colors.MediumBlue};  
-//   }
-// `;
-
 
